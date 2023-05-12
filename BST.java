e489729d89b79c0720655b60e67282ad3213c513
Dek <karen.ananyan2017@mail.ru>
2023-05-12 20:37:56 +0600

    Put method, Size implementatiion

--- BST.java
+++ BST.java
@@ -4,16 +4,35 @@ public class BST<K extends Comparable<K>, V> {
     {
         private K key;
         private V val;
+        private int size;
         private Node left, right;
-        public Node(K key, V val)
+        public Node(K key, V val, int size)
         {
             this.key = key;
             this.val = val;
+            this.size = size;
         }
     }
     public void put(K key, V val)
     {
+        root = put(root, key, val);
+    }
 
+    private Node put(Node n,K key, V val)
+    {
+        if (n == null){
+            return new Node(key, val, 1);
+        }
+        int cmp = key.compareTo(n.key);
+        if (cmp < 0) {
+            n.left = put(n.left, key, val);
+        }
+        else if (cmp > 0) {
+            n.right = put(n.right, key, val);
+        }
+        else n.val = val;
+        n.size++;
+        return n;
     }
 
     public  V get(K key)
